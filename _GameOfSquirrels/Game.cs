82fb1cdc2aac7560ca8f70281c5c0c77aeb8e1c5
Joyce A <joyce.alfvoet@gmail.com>
2021-02-11 20:58:02 +0100

    Cleaned up MainWindow to game class

--- _GameOfSquirrels/Game.cs
+++ _GameOfSquirrels/Game.cs
@@ -1,6 +1,62 @@
-﻿namespace _GameOfSquirrels
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace _GameOfSquirrels
 {
-    internal class Game
+   public class Game
     {
+        private Dice dice = new Dice();
+        private Board board;
+        public List<IPawn> Playerlist;
+        public List<ITile> BoardTiles;
+
+        public Grid GridGame { get; set; }
+
+        public Game(Grid gridGame)
+        {
+            GridGame = gridGame;
+        }
+
+        public void GamePlay()
+        {
+            board = new Board(GridGame, 0, 20);
+            GridGame.ShowGridLines = true;
+            TileFactory tileFactory = new TileFactory();
+            BoardTiles = tileFactory.CreateTiles();
+            foreach (var tile in BoardTiles)
+            {
+                GridGame.Children.Add(tile.TileBorder);
+                Grid.SetColumn(tile.TileBorder, tile.LocationX);
+            }
+
+            Pawn pawn1 = new Pawn(3, 1);
+            Pawn pawn2 = new Pawn(1, 1);
+            Playerlist = new List<IPawn> { pawn1, pawn2 };
+            foreach (var item in Playerlist)
+            {
+                GridGame.Children.Add(item.Ellipse);
+                Grid.SetColumn(item.Ellipse, item.LocationX);
+                Grid.SetRow(item.Ellipse, item.LocationY);
+            }
+
+        }
+
+        public void MovePawn(int move)
+        {
+            Playerlist[0].Move(move);
+            foreach (var item in BoardTiles)
+            {
+                if (Playerlist[0].LocationX == item.LocationX)
+                {
+                    MovePawn(item.GetInteraction());
+                }
+            }
+
+            if (Playerlist[0].LocationX >= 20)
+            {
+                Playerlist[0].LocationX = 0;
+                Grid.SetColumn(Playerlist[0].Ellipse, Playerlist[0].LocationX);
+            }
+        }
     }
 }
--- _GameOfSquirrels/MainWindow.xaml.cs
+++ _GameOfSquirrels/MainWindow.xaml.cs
@@ -9,61 +9,25 @@ namespace _GameOfSquirrels
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Dice dice = new Dice();
-        private Board board;
-        public List<IPawn> Playerlist;
-        public List<ITile> BoardTiles;
+        public Game game;
 
         public MainWindow()
         {
             InitializeComponent();
-            board = new Board(GridGame, 0, 20);
-            GridGame.ShowGridLines = true;
-            //GridGame.Height = 30;
-            TileFactory tileFactory = new TileFactory();
-            BoardTiles = tileFactory.CreateTiles();
-            foreach (var tile in BoardTiles)
-            {
-                GridGame.Children.Add(tile.TileBorder);
-                Grid.SetColumn(tile.TileBorder, tile.LocationX);
-            }
+            game = new Game(GridGame);
+            game.GamePlay();
 
-            //CatapultTile testtile = new CatapultTile(5, 5);
-            //BoardTiles.Add(testtile);
-            //GridGame.Children.Add(testtile.TileBorder);
-            //Grid.SetColumn(testtile.TileBorder, testtile.LocationX);
-
-            Pawn pawn = new Pawn(1, 1);
-            GridGame.Children.Add(pawn.Ellipse);
-            Grid.SetColumn(pawn.Ellipse, pawn.LocationX);
-            Grid.SetRow(pawn.Ellipse, pawn.LocationY);
-            Playerlist = new List<IPawn> { pawn };
         }
 
         private void TestButtonClick(object sender, RoutedEventArgs e)
         {
             Dice dice = new Dice();
             int result = dice.RollDice(1, 7);
-            MovePawn(result);
+            Dicelabel.Content = result;
+            game.MovePawn(result);
+
         }
 
-        public void MovePawn(int move)
-        {
-            Playerlist[0].Move(move);
-            Dicelabel.Content = Playerlist[0].LastRoll;
-            foreach (var item in BoardTiles)
-            {
-                if (Playerlist[0].LocationX == item.LocationX)
-                {
-                    MovePawn(item.GetInteraction());
-                }
-            }
-
-            if (Playerlist[0].LocationX >= 20)
-            {
-                Playerlist[0].LocationX = 0;
-                Grid.SetColumn(Playerlist[0].Ellipse, Playerlist[0].LocationX);
-            }
-        }
+       
     }
 }
